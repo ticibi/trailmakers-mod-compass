f4616de3a27e8f99f2a5b641bab05cbcdf2a97eb
ticibi <tcb003@outlook.com>
2021-09-28 16:16:48 -0400

    update

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 -- Compass
--- by dinoman/ticibi 2021
+-- TrailMakers Mod by dinoman/ticib 2021
 
 local debug = false
 local globalTime = 0
@@ -7,16 +7,15 @@ local compass = "N....................E....................S....................
 
 function onPlayerJoined(player)
     tm.os.Log(tm.players.GetPlayerName(player.playerId) .. " joined the server")
-    initializeUI_AndKeybinds(player.playerId)
+    initializeUI(player.playerId)
 end
 
 function onPlayerLeft(player)
     tm.os.Log(tm.players.GetPlayerName(player.playerId) .. " left the server")
 end
 
-function initializeUI_AndKeybinds(playerId)
+function initializeUI(playerId)
     homePage(playerId)
-    --tm.input.RegisterFunctionToKeyDownCallback(playerId, "" ,"")
 end
 
 tm.players.OnPlayerJoined.add(onPlayerJoined)
@@ -24,10 +23,12 @@ tm.players.OnPlayerLeft.add(onPlayerLeft)
 
 function update()
     local playerList = tm.players.CurrentPlayers()
-    for k, player in pairs(playerList) do
+    for _, player in pairs(playerList) do
         updateCompass(player.playerId)
-        if debug then globalTime = globalTime + 1 end
-        if debug then tm.playerUI.SetUIValue(player.playerId, "globaltime", "time: " .. globalTime/10) end
+        if debug then 
+            globalTime = globalTime + 1
+            tm.playerUI.SetUIValue(player.playerId, "globaltime", "time: " .. globalTime/10) 
+        end
     end
 end
 
@@ -35,6 +36,28 @@ function updateCompass(playerId)
     local transform = tm.players.GetPlayerTransform(playerId)
     local rotation = transform.GetRotation()
     local heading = rotation.y
+    local spacer = updateSpacer()
+    local rate = 360/#compass 
+    local hdgIndex = math.floor(heading / rate)
+    local minValue = hdgIndex - 20
+    local maxValue = hdgIndex + 22
+    local displayText = ""
+    if hdgIndex + 20 > #compass then
+        local diff = hdgIndex + 20 - (#compass)
+        local buffer = string.sub(compass, 1, diff)
+        displayText = string.sub(compass, minValue, #compass) .. buffer
+    elseif hdgIndex - 20 < 1 then
+        local diff = math.abs(hdgIndex - 20)
+        local buffer = string.sub(compass, #compass-diff, #compass)
+        displayText = buffer .. string.sub(compass, 1, maxValue)
+    else
+        displayText = string.sub(compass, minValue, maxValue)
+    end
+    tm.playerUI.SetUIValue(playerId, "heading", math.floor(heading))
+    tm.playerUI.SetUIValue(playerId, "compass", displayText)  
+end
+
+function updateSpacer()
     local spacer = ""
     if math.floor(heading) > 9 and math.floor(heading) < 100 then
         spacer = "                      "
@@ -43,24 +66,7 @@ function updateCompass(playerId)
     else
         spacer = "                       "
     end
-    local rate = 360/#compass 
-    local hdgIndex = math.floor(heading / rate)
-    local minValue = hdgIndex - 20
-    local maxValue = hdgIndex + 20
-    local display = ""
-    if hdgIndex + 20 > #compass then
-        local diff = hdgIndex + 20 - (#compass)
-        local buffer = string.sub(compass, 1, diff)
-        display = string.sub(compass, minValue, #compass) .. buffer
-    elseif hdgIndex - 20 < 1 then
-        local diff = math.abs(hdgIndex - 20)
-        local buffer = string.sub(compass, #compass-diff, #compass)
-        display = buffer .. string.sub(compass, 1, maxValue+2)
-    else
-        display = string.sub(compass, minValue, maxValue+2)
-    end
-    tm.playerUI.SetUIValue(playerId, "heading", math.floor(heading))
-    tm.playerUI.SetUIValue(playerId, "compass", display)  
+    return spacer
 end
 
 function homePage(playerId)
